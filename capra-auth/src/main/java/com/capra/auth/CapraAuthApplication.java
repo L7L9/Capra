3de2fbbc60f230598af6b51ad66d578c02a128e0
lql <1964757607@qq.com>
2023-10-23 20:55:00 +0800

    init(auth):初始化auth模块

---
+++ capra-auth/src/main/java/com/capra/auth/CapraAuthApplication.java
@@ -0,0 +1,16 @@
+package com.capra.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lql
+ * @date 2023/10/23
+ */
+@SpringBootApplication
+
+public class CapraAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapraAuthApplication.class,args);
+    }
+}
