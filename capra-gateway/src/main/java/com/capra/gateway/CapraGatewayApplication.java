1c2f47d371c24f02cd19a2ebecab1c3399163ac5
lql <1964757607@qq.com>
2023-10-23 17:48:19 +0800

    init:网关模块初始化

---
+++ capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.capra.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author lql
+ * @date 2023/10/23
+ */
+@SpringBootApplication
+public class CapraGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapraGatewayApplication.class,args);
+    }
+}
