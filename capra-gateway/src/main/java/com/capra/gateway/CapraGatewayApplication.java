58546fef6d7ac9f0888f73834bbe215caa1520db
lql <1964757607@qq.com>
2023-10-23 20:54:07 +0800

    feat(gateway):添加nacos配置依赖和服务发现依赖

--- capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
+++ capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
@@ -2,6 +2,7 @@ package com.capra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  *
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023/10/23
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CapraGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraGatewayApplication.class,args);
