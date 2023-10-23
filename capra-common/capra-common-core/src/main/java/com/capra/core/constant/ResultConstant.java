c45a62ab8d56f1e2c9f7fdeaf4a5248e98546fb5
lql <1964757607@qq.com>
2023-10-23 20:56:01 +0800

    feat(core/constant):添加返回结果的状态码常量类

---
+++ capra-common/capra-common-core/src/main/java/com/capra/core/constant/ResultConstant.java
@@ -0,0 +1,17 @@
+package com.capra.core.constant;
+
+/**
+ * @author lql
+ * @date 2023/10/23
+ */
+public class ResultConstant {
+    /**
+     * 结果操作成功的状态码
+     */
+    public static final int SUCCESS_CODE = 200;
+
+    /**
+     * 结果操作失败的状态码
+     */
+    public static final int FAILED_CODE = 500;
+}
