beba7e448869772e3103287e3b536692b6520588
lql <1964757607@qq.com>
2023-10-24 22:40:08 +0800

    feat(account.controller):添加AccountController类

---
+++ capra-account/src/main/java/com/capra/account/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.capra.account.controller;
+
+import com.capra.account.entity.dto.RegisterDTO;
+import com.capra.account.service.AccountService;
+import com.capra.core.result.CommonResult;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 账户controller
+ *
+ * @author lql
+ * @date 2023/10/24
+ */
+@RestController
+public class AccountController {
+    @Resource
+    private AccountService accountService;
+
+    @PostMapping("register")
+    public CommonResult<?> register(@RequestBody RegisterDTO registerDTO){
+        if(accountService.register(registerDTO)){
+            return CommonResult.successWithMeg("注册成功");
+        }
+        return CommonResult.fail();
+    }
+}
