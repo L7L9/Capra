8b8a2a0ce7d89c84d19ac234ed93dcf5876869ca
lql <1964757607@qq.com>
2023-10-24 20:24:37 +0800

    delete(common):删除lombok依赖以及@Data注解

--- capra-common/capra-common-core/src/main/java/com/capra/core/result/CommonResult.java
+++ capra-common/capra-common-core/src/main/java/com/capra/core/result/CommonResult.java
@@ -1,7 +1,6 @@
 package com.capra.core.result;
 
 import com.capra.core.constant.ResultConstant;
-import lombok.Data;
 import org.apache.logging.log4j.util.Strings;
 
 import java.io.Serializable;
@@ -10,7 +9,6 @@ import java.io.Serializable;
  * @author lql
  * @date 2023/10/23
  */
-@Data
 public class CommonResult<T> implements Serializable {
     /**
      * 状态码
@@ -65,4 +63,27 @@ public class CommonResult<T> implements Serializable {
         return new CommonResult<>(ResultConstant.FAILED_CODE,message,data);
     }
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
