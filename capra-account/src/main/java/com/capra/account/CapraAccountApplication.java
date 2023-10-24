872d4ac9f5e264fd237398cd4cc37d401d431e32
lql <1964757607@qq.com>
2023-10-24 20:23:55 +0800

    init(account):初始化account模块

---
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -0,0 +1,16 @@
+package com.capra.account;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lql
+ * @date 2023/10/24
+ */
+@SpringBootApplication
+public class CapraAccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapraAccountApplication.class,args);
+    }
+}
