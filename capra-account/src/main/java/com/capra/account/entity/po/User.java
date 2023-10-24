bfdb0c4daac05fb4cb98ce6552897454e59dcce7
lql <1964757607@qq.com>
2023-10-24 22:27:21 +0800

    feat(account.po):添加用户表对应的po类

---
+++ capra-account/src/main/java/com/capra/account/entity/po/User.java
@@ -0,0 +1,75 @@
+package com.capra.account.entity.po;
+
+/**
+ * 用户po
+ *
+ * @author lql
+ * @date 2023/10/24
+ */
+
+public class User {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 全局唯一标识
+     */
+    private String uuid;
+
+    /**
+     * 账户名称
+     */
+    private String username;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 头像
+     */
+    private String headImg;
+
+    /**
+     * 积分数量
+     */
+    private Long points;
+
+    /**
+     * 自我介绍
+     */
+    private String description;
+
+    /**
+     * 文章数量
+     */
+    private Long articleCount;
+
+    /**
+     * 关注用户数量
+     */
+    private Long followCount;
+
+    /**
+     * 粉丝数量
+     */
+    private Long fansCount;
+
+    /**
+     * 账户状态
+     */
+    private Integer status;
+}
