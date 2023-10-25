0eb09d3cdc0e5c5669e2db5602bd20777140f156
lql <1964757607@qq.com>
2023-10-25 19:53:21 +0800

    feat(account.dto):添加参数校验的参数

--- capra-account/src/main/java/com/capra/account/entity/dto/RegisterDTO.java
+++ capra-account/src/main/java/com/capra/account/entity/dto/RegisterDTO.java
@@ -1,6 +1,8 @@
 package com.capra.account.entity.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * 注册dto
@@ -13,10 +15,14 @@ public class RegisterDTO {
     /**
      * 用户名
      */
+    @NotBlank
+    @Length(min = 2,max = 20)
     private String username;
 
     /**
      * 密码
      */
+    @NotBlank
+    @Length(min = 6,max = 20)
     private String password;
 }
