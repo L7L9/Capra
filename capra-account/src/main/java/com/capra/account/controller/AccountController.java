09787dd1d552530d984a734577aa3cf56e19224d
lql <1964757607@qq.com>
2023-10-25 19:48:52 +0800

    feat(account.controller):修复路径错误的bug

--- capra-account/src/main/java/com/capra/account/controller/AccountController.java
+++ capra-account/src/main/java/com/capra/account/controller/AccountController.java
@@ -7,6 +7,7 @@ import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/10/24
  */
 @RestController
+@RequestMapping("account")
 public class AccountController {
     @Resource
     private AccountService accountService;
 
-    @PostMapping("register")
+    @PostMapping("/register")
     public CommonResult<?> register(@Validated @RequestBody RegisterDTO registerDTO){
         if(accountService.register(registerDTO)){
             return CommonResult.successWithMeg("注册成功");
