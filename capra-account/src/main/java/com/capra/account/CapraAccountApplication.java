a9b9030c695e1fd2c152cfc86949a8eae9f82f01
lql <1964757607@qq.com>
2023-10-25 13:35:48 +0800

    feat(account):更新依赖

--- capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -1,6 +1,7 @@
 package com.capra.account;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lql
  * @date 2023/10/24
  */
+@MapperScan("com.capra.account.mapper")
 @SpringBootApplication
 public class CapraAccountApplication {
     public static void main(String[] args) {
