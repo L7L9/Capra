275fe681644d2ddc430fe2de2d99f30db8017d22
lql <1964757607@qq.com>
2023-10-25 13:37:03 +0800

    feat(account.mapper):添加UserMapper

---
+++ capra-account/src/main/java/com/capra/account/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.capra.account.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.capra.account.entity.po.User;
+
+/**
+ * @author lql
+ * @date 2023/10/25
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
