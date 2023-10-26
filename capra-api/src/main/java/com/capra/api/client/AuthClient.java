27ad8e2fc99a3d2baf424cae5a36dcc2df13b61c
lql <1964757607@qq.com>
2023-10-26 22:07:17 +0800

    feat(api.client):定义getToken远程调用接口

---
+++ capra-api/src/main/java/com/capra/api/client/AuthClient.java
@@ -0,0 +1,24 @@
+package com.capra.api.client;
+
+import com.capra.api.domain.SysUser;
+import com.capra.core.result.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * 定义Auth模块的远程接口
+ *
+ * @author lql
+ * @date 2023/10/26
+ */
+@FeignClient("capra-auth")
+public interface AuthClient {
+    /**
+     * 获取token
+     *
+     * @param sysUser 存于jwt的用户信息
+     * @return jwt
+     */
+    @PostMapping("getToken")
+    CommonResult<String> getToken(SysUser sysUser);
+}
