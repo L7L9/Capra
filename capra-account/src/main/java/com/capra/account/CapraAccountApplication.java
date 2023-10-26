9d4e6f05bcdf5b620c1df798146b1a1a7d8edb96
lql <1964757607@qq.com>
2023-10-26 22:05:14 +0800

    feat(account):添加@EnableDiscoveryClient注解

--- capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -4,6 +4,7 @@ package com.capra.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author lql
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("com.capra.account.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CapraAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraAccountApplication.class,args);
