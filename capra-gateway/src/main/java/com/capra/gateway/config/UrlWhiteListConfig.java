8e1649f07d91b46946f5324ad8e81893afc0d64f
lql <1964757607@qq.com>
2023-10-26 20:04:25 +0800

    feat(gateway.config):添加url白名单

---
+++ capra-gateway/src/main/java/com/capra/gateway/config/UrlWhiteListConfig.java
@@ -0,0 +1,21 @@
+package com.capra.gateway.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * URL的白名单
+ * 不需要鉴权，直接放行
+ *
+ * @author lql
+ * @date 2023/10/26
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix="security.ignore")
+public class UrlWhiteListConfig {
+    private List<String> urls;
+}
