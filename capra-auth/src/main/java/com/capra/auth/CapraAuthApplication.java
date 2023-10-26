10df9dec6e92e5d4cd34bb1399f8e9858f9eab16
lql <1964757607@qq.com>
2023-10-26 22:05:05 +0800

    feat(auth):添加@EnableDiscoveryClient注解

--- capra-auth/src/main/java/com/capra/auth/CapraAuthApplication.java
+++ capra-auth/src/main/java/com/capra/auth/CapraAuthApplication.java
@@ -2,13 +2,14 @@ package com.capra.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author lql
  * @date 2023/10/23
  */
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class CapraAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraAuthApplication.class,args);
