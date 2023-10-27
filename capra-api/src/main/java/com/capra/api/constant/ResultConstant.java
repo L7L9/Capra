d7e08f41b2412813595e3634bf455c63bfa54583
lql <1964757607@qq.com>
2023-10-27 14:01:12 +0800

    feat(api.constant):添加远程调用结果码常量类

---
+++ capra-api/src/main/java/com/capra/api/constant/ResultConstant.java
@@ -0,0 +1,13 @@
+package com.capra.api.constant;
+
+/**
+ * 远程调用结果常量类
+ *
+ * @author lql
+ * @date 2023/10/27
+ */
+public class ResultConstant {
+    public static final int SUCCESS = 100;
+
+    public static final int FAILED = 400;
+}
