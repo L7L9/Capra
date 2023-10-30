e0dd53320bed911c310e539ee4f3eae66cea8fbb
lql <1964757607@qq.com>
2023-10-30 20:45:39 +0800

    feat(constant):添加请求头常量

---
+++ capra-gateway/src/main/java/com/capra/gateway/constant/HeaderConstant.java
@@ -0,0 +1,11 @@
+package com.capra.gateway.constant;
+
+/**
+ * 请求头常量
+ *
+ * @author lql
+ * @date 2023/10/29
+ */
+public class HeaderConstant {
+    public static final String TOKEN_HEADER = "Token";
+}
