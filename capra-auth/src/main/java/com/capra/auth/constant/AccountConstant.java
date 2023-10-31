477e9f56d4367495270f89c2edfeb84c4449181b
lql <1964757607@qq.com>
2023-10-31 16:42:15 +0800

    feat(client):添加表示账户状态的常量类

---
+++ capra-auth/src/main/java/com/capra/auth/constant/AccountConstant.java
@@ -0,0 +1,30 @@
+package com.capra.auth.constant;
+
+/**
+ * 账户状态的常量类
+ *
+ * @author lql
+ * @date 2023/10/31
+ */
+public class AccountConstant {
+    /**
+     * 正常状态
+     */
+    public final static int NORMAL = 0;
+
+    /**
+     * 注销状态
+     */
+    public final static int LOG_OFF = 1;
+
+    /**
+     * 冻结状态
+     */
+    public final static int FREEZE = 2;
+
+    /**
+     * 其他状态
+     */
+    public final static int OTHER = 3;
+
+}
