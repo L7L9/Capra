22f8fd21b33bbddbe978a14e6f8a13ffbe273d08
lql <1964757607@qq.com>
2023-10-31 15:43:47 +0800

    feat(utils):添加判断字符串长度在某区间的方法

--- capra-common/capra-common-core/src/main/java/com/capra/core/utils/StringUtils.java
+++ capra-common/capra-common-core/src/main/java/com/capra/core/utils/StringUtils.java
@@ -25,4 +25,22 @@ public class StringUtils {
     public static boolean notEmpty(String s){
         return !isEmpty(s);
     }
+
+    /**
+     * 判断字符串长度是否符合: min <= s.len <= max
+     * @param s 字符串
+     * @param min 最小值
+     * @param max 最大值
+     * @return boolean 符合返回true,否则返回false
+     */
+    public static boolean lenBetween(String s,int min,int max){
+        if(min < 0 || max < 0 || min > max){
+            throw new RuntimeException("参数越界");
+        }
+        int length = s.length();
+        if(length >= min && length <= max){
+            return true;
+        }
+        return false;
+    }
 }
