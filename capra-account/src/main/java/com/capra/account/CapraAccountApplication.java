cc76f2c2ec7d9eafa189b06a899b9cdc8358af8b
lql <1964757607@qq.com>
2023-10-31 16:43:37 +0800

    delete(account):删除@EnableFeignClient注解

--- capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -1,12 +1,10 @@
 package com.capra.account;
 
 
-import com.capra.api.client.AuthClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lql
@@ -15,7 +13,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @MapperScan("com.capra.account.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = AuthClient.class)
 public class CapraAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraAccountApplication.class,args);
