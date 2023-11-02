62957996fa7ac6f14395a7e2a81db6a0b23f034b
lql <1964757607@qq.com>
2023-11-02 15:02:40 +0800

    feat(service):添加redis服务类

---
+++ capra-common/capra-common-redis/src/main/java/com/capra/redis/service/RedisService.java
@@ -0,0 +1,55 @@
+package com.capra.redis.service;
+
+/**
+ * redis服务接口
+ *
+ * @author lql
+ * @date 2023/11/02
+ */
+public interface RedisService {
+    /**
+     * 设置属性
+     *
+     * @param key 键
+     * @param value 值
+     */
+    void set(String key,Object value);
+
+    /**
+     * 设置属性以及过期时间
+     *
+     * @param key 键
+     * @param value 值
+     * @param ttl 过期时间
+     */
+    void set(String key,Object value,long ttl);
+
+    /**
+     * 获取键对应的值
+     * @param key 键
+     * @return 值
+     */
+    Object get(String key);
+
+    /**
+     * 删除键
+     * @param key 键
+     * @return 删除成功返回true
+     */
+    Boolean delete(String key);
+
+    /**
+     * 设置过期时间
+     * @param key 键
+     * @param ttl 过期时间
+     * @return 成功返回true
+     */
+    Boolean setExpire(String key,long ttl);
+
+    /**
+     * 获取过期时间
+     * @param key 键
+     * @return 过期时间
+     */
+    Long getExpire(String key);
+}
---
+++ capra-common/capra-common-redis/src/main/java/com/capra/redis/service/impl/RedisServiceImpl.java
@@ -0,0 +1,51 @@
+package com.capra.redis.service.impl;
+
+import com.capra.redis.service.RedisService;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * redis服务接口的实现类
+ *
+ * @author lql
+ * @date 2023/11/02
+ */
+@Component
+public class RedisServiceImpl implements RedisService {
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
+
+    @Override
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    @Override
+    public void set(String key, Object value, long ttl) {
+        redisTemplate.opsForValue().set(key, value, ttl, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public Boolean delete(String key) {
+        return redisTemplate.delete(key);
+    }
+
+    @Override
+    public Boolean setExpire(String key, long ttl) {
+        return redisTemplate.expire(key,ttl,TimeUnit.SECONDS);
+    }
+
+    @Override
+    public Long getExpire(String key) {
+        return redisTemplate.getExpire(key);
+    }
+}
