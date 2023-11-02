6cbefda61d67f67e1da1b519ed15c2b2715c9556
lql <1964757607@qq.com>
2023-11-02 22:03:06 +0800

    feat(constant):添加Token有关的常量类

---
+++ capra-auth/src/main/java/com/capra/auth/constant/TokenConstant.java
@@ -0,0 +1,14 @@
+package com.capra.auth.constant;
+
+/**
+ * token常量
+ *
+ * @author lql
+ * @date 2023/11/02
+ */
+public class TokenConstant {
+    /**
+     * ttl => 2天
+     */
+    public static final long TTL = 60 * 60 * 2;
+}
