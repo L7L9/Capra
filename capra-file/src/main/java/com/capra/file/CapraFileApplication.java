4060f20d8bf5185e7cf2ea77d20b2b55aef99edc
lql <1964757607@qq.com>
2023-11-07 21:42:06 +0800

    init:初始化文件模块

---
+++ capra-file/src/main/java/com/capra/file/CapraFileApplication.java
@@ -0,0 +1,17 @@
+package com.capra.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 文件模块启动类
+ *
+ * @author lql
+ * @date 2023/11/07
+ */
+@SpringBootApplication
+public class CapraFileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapraFileApplication.class,args);
+    }
+}
