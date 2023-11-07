e0829f0d246c20fc8e65aaee6aa07b33955eb610
lql <1964757607@qq.com>
2023-11-07 21:44:36 +0800

    init:初始化文章模块

---
+++ capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
@@ -0,0 +1,16 @@
+package com.capra.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 文章模块启动类
+ * @author lql
+ * @date 2023/11/07
+ */
+@SpringBootApplication
+public class CapraArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CapraArticleApplication.class,args);
+    }
+}
