cfe80e2e9ab95ce704d9d0b2c5585565e23511ad
lql <1964757607@qq.com>
2023-11-10 16:04:48 +0800

    feat:添加@MapperScan注解

--- capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
+++ capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
@@ -1,5 +1,6 @@
 package com.capra.article;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lql
  * @date 2023/11/07
  */
+@MapperScan("com.capra.article.mapper")
 @SpringBootApplication
 public class CapraArticleApplication {
     public static void main(String[] args) {
