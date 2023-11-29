1748ae6bab95d035666a48261bc5144981d165d8
lql <1964757607@qq.com>
2023-11-29 17:19:59 +0800

    feat(constant):添加minioBucket常量类

---
+++ capra-common/capra-common-core/src/main/java/com/capra/core/constant/MinioBucketConstant.java
@@ -0,0 +1,14 @@
+package com.capra.core.constant;
+
+/**
+ * minio bucket名字
+ *
+ * @author lql
+ * @date 2023/11/28
+ */
+public class MinioBucketConstant {
+    /**
+     * 文章类型的文件桶
+     */
+    public static final String ARTICLE = "capra-article";
+}
