3a2744386f6db50061fe7aa36fa55761a7e13205
lql <1964757607@qq.com>
2023-11-29 17:19:27 +0800

    feat(config):添加minio配置类

---
+++ capra-file/src/main/java/com/capra/file/config/MinioConfig.java
@@ -0,0 +1,39 @@
+package com.capra.file.config;
+
+import io.minio.MinioClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * minio文件存储客户端配置
+ *
+ * @author lql
+ * @date 2023/11/27
+ */
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+    /**
+     * 服务地址
+     */
+    private String endpoint;
+
+    /**
+     * 用户key
+     */
+    private String accessKey;
+
+    /**
+     * 密码key
+     */
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient(){
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey,secretKey)
+                .build();
+    }
+}
