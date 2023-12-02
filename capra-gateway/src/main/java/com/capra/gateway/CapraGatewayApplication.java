1849c51eb7affbe6b87f73e13d0d4ad042236069
lql <1964757607@qq.com>
2023-12-02 15:27:54 +0800

    feat:添加注入authClient

--- capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
+++ capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.capra.gateway;
 
+import com.capra.api.client.AuthClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  *
@@ -11,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(clients = AuthClient.class)
 public class CapraGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraGatewayApplication.class,args);
