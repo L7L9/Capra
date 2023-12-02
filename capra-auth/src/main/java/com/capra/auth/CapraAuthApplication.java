6012255b79b0ad84219103973c80b08e3ad003df
lql <1964757607@qq.com>
2023-12-02 15:27:32 +0800

    fix:修改无法注入redis服务的bug

--- capra-auth/src/main/java/com/capra/auth/CapraAuthApplication.java
+++ capra-auth/src/main/java/com/capra/auth/CapraAuthApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author lql
  * @date 2023/10/23
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.capra")
 @EnableDiscoveryClient
 @EnableFeignClients(clients = UserClient.class)
 public class CapraAuthApplication {
