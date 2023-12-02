6af2c9e27bf135a72afb36f024b98e295713529d
lql <1964757607@qq.com>
2023-12-02 15:26:57 +0800

    feat:添加fileClient类的注入

--- capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
+++ capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
@@ -1,8 +1,10 @@
 package com.capra.article;
 
+import com.capra.api.client.FileClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 文章模块启动类
@@ -11,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("com.capra.article.mapper")
 @SpringBootApplication
+@EnableFeignClients(clients = FileClient.class)
 public class CapraArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraArticleApplication.class,args);
