80dbbb808389ec15f39b6ea8538bd8cebb9b1842
lql <1964757607@qq.com>
2023-12-03 20:30:04 +0800

    feat:修改容器扫描包

--- capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
+++ capra-gateway/src/main/java/com/capra/gateway/CapraGatewayApplication.java
@@ -1,10 +1,9 @@
 package com.capra.gateway;
 
-import com.capra.api.client.AuthClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
@@ -12,8 +11,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2023/10/23
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.capra")
 @EnableDiscoveryClient
-@EnableFeignClients(clients = AuthClient.class)
 public class CapraGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraGatewayApplication.class,args);
