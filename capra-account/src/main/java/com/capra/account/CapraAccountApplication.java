67823938f8367b76f9b910252e21952a0cc3fa2f
lql <1964757607@qq.com>
2023-12-04 23:02:08 +0800

    feat:注入fileClient

--- capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -1,10 +1,12 @@
 package com.capra.account;
 
 
+import com.capra.api.client.FileClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lql
@@ -13,6 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan("com.capra.account.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(clients = {FileClient.class})
 public class CapraAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(CapraAccountApplication.class,args);
