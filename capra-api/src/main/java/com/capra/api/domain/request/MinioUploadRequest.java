1bafe6dfa8e0b03baddec1e088854d7f22564575
lql <1964757607@qq.com>
2023-12-04 22:59:26 +0800

    feat(request):添加自定义文件名

--- capra-api/src/main/java/com/capra/api/domain/request/MinioUploadRequest.java
+++ capra-api/src/main/java/com/capra/api/domain/request/MinioUploadRequest.java
@@ -1,6 +1,7 @@
 package com.capra.api.domain.request;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -8,8 +9,20 @@ import org.springframework.web.multipart.MultipartFile;
  * @date 2023/11/29
  */
 @Data
+@Accessors(chain = true)
 public class MinioUploadRequest {
+    /**
+     * 文件内容
+     */
     public MultipartFile file;
 
+    /**
+     * 自定义文件名
+     */
+    private String filename;
+
+    /**
+     * 文件桶名
+     */
     private String bucketName;
 }
