769ed065d3d8e75ddbe47ddfa51eddd2aada5233
lql <1964757607@qq.com>
2023-12-04 22:31:40 +0800

    feat(mapper):添加通过title和authorId判断数据是否存在的方法

--- capra-article/src/main/java/com/capra/article/mapper/ArticleMetadataMapper.java
+++ capra-article/src/main/java/com/capra/article/mapper/ArticleMetadataMapper.java
@@ -1,5 +1,6 @@
 package com.capra.article.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.capra.article.domain.po.ArticleMetadata;
@@ -21,5 +22,18 @@ public interface ArticleMetadataMapper extends BaseMapper<ArticleMetadata> {
         return this.update(new ArticleMetadata().setGroupId(transferId),
                 new UpdateWrapper<ArticleMetadata>().lambda()
                         .eq(ArticleMetadata::getGroupId,sourceId));
+    }
+
+    /**
+     * 判断title是否重复
+     *
+     * @param title    文章标题
+     * @param authorId 作者id
+     * @return 存在返回true, 否则返回false
+     */
+    default Boolean existsByTitleAndAuthorId(String title,Long authorId){
+        return this.exists(new QueryWrapper<ArticleMetadata>().lambda()
+                .eq(ArticleMetadata::getTitle,title)
+                .eq(ArticleMetadata::getAuthorId,authorId));
     }
 }
