ac4e75bbfb547f66437c35185b2c1e8205e62dd3
lql <1964757607@qq.com>
2023-12-05 23:42:38 +0800

    feat:添加扫面包注解

--- capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
+++ capra-account/src/main/java/com/capra/account/CapraAccountApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author lql
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @MapperScan("com.capra.account.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "com.capra")
 @EnableFeignClients(clients = {FileClient.class})
 public class CapraAccountApplication {
     public static void main(String[] args) {
--- capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
+++ capra-article/src/main/java/com/capra/article/CapraArticleApplication.java
@@ -4,7 +4,9 @@ import com.capra.api.client.FileClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 文章模块启动类
@@ -13,6 +15,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @MapperScan("com.capra.article.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.capra")
 @EnableFeignClients(clients = FileClient.class)
 public class CapraArticleApplication {
     public static void main(String[] args) {
