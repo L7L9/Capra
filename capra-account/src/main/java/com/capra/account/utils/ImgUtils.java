0c1c2a61d99339260cac68ca25a37382a959a450
lql <1964757607@qq.com>
2023-12-06 16:49:55 +0800

    refactor(utils):删除toBase64方法以及修改getType方法为public

--- capra-account/src/main/java/com/capra/account/utils/ImgUtils.java
+++ capra-account/src/main/java/com/capra/account/utils/ImgUtils.java
@@ -1,10 +1,7 @@
 package com.capra.account.utils;
 
-import cn.hutool.core.codec.Base64;
-import com.capra.core.exception.SystemException;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Objects;
 
 /**
@@ -18,35 +15,6 @@ public class ImgUtils {
      * 图片文件的后缀
      */
     private static final String[] FILE_SUFFIX = new String[]{"jpg","png","jpeg","pjg","pjeg","jfif"};
-
-    /**
-     * 数据前缀
-     */
-    private static final String DATA_PREFIX = "data:image/";
-
-    /**
-     * 编码类型 => base64
-     */
-    private static final String CODE_TYPE = ";base64";
-
-    /**
-     * 使用base64对图片数据编码
-     *
-     * @param file 图片文件
-     * @return 返回编码后的字符串
-     */
-    public static String toBase64(MultipartFile file){
-        if(Objects.isNull(file)){
-            throw new SystemException("文件不存在");
-        }
-        StringBuilder stringBuilder = new StringBuilder(DATA_PREFIX);
-        try {
-             stringBuilder.append(getType(file)).append(CODE_TYPE).append(Base64.encode(file.getBytes()));
-        } catch (IOException e) {
-            throw new RuntimeException("处理图片文件失败");
-        }
-        return stringBuilder.toString();
-    }
 
     /**
      * 检测是否为图片
@@ -70,7 +38,7 @@ public class ImgUtils {
      * @param file 文件
      * @return 返回后缀
      */
-    private static String getType(MultipartFile file){
-        return Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().indexOf(".") + 1);
+    public static String getType(MultipartFile file){
+        return Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().indexOf("."));
     }
 }
