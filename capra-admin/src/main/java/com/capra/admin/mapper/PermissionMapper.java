0c93086d2c888650eda23a865d8ac1c4bd57021b
lql <1964757607@qq.com>
2023-12-07 17:16:32 +0800

    feat(mapper):添加role&&permission表的mapper类

---
+++ capra-admin/src/main/java/com/capra/admin/mapper/PermissionMapper.java
@@ -0,0 +1,13 @@
+package com.capra.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.capra.admin.domain.po.Permission;
+
+/**
+ * permission表对应的mapper
+ *
+ * @author lql
+ * @date 2023/12/07
+ */
+public interface PermissionMapper extends BaseMapper<Permission> {
+}
---
+++ capra-admin/src/main/java/com/capra/admin/mapper/RoleMapper.java
@@ -0,0 +1,13 @@
+package com.capra.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.capra.admin.domain.po.Role;
+
+/**
+ * role表对应mapper
+ *
+ * @author lql
+ * @date 2023/12/07
+ */
+public interface RoleMapper  extends BaseMapper<Role> {
+}
